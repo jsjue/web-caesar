e34f54a052389ce453214edd6237ea52ff0de45b
Janelle Sjue <janellesjue@gmail.com>
2018-10-17 21:40:13 -0500

    added code

---
+++ LC101 projects/main.py
@@ -0,0 +1,46 @@
+
+
+from flask import Flask, request
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <form method="POST">
+            <label for "rot"> Rotate by:
+                <input type="text" name="rot" value="0"/>
+            </label>
+        <textarea> name="text">{0}</textarea>
+        <input type ="submit"/>
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form.format("")
+
+app.run()
